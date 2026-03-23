78441f676a42959ec5b1c7aae1f0b8b962608b89
Leheza Daniil <lehesadanylo27@gmail.com>
2026-03-23 23:12:50 +0200

    feat: add JWT initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
-from app.db import db
+from app.db import db, jwt
 from app.routes.auth import auth
 # from app.routes.locations import locations
 # from app.routes.sensors import sensors
@@ -11,7 +11,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-
+    jwt.init_app(app)
+    
     app.register_blueprint(auth)
     # app.register_blueprint(locations)
     # app.register_blueprint(sensors)
