ccc0d92b503ecac07743651d09642f8465207b3b
Leheza Daniil <lehesadanylo27@gmail.com>
2026-03-28 21:35:12 +0200

    feat(tests): add unit tests of validation telemetry value

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -12,5 +12,17 @@ def test_email_empty():
 def test_email_no_at():
     assert is_valid_email("coolEmailButnoAt") is False
 
+def test_valid_telemetry_value():
+    assert is_valid_telemetry_value(100) is True
+
+def test_negative_telemetry_value():
+    assert is_valid_telemetry_value(-1) is False
+
+def test_too_big_temeletry_value():
+    assert is_valid_telemetry_value(101) is False
+
+def test_NaN_telemetry_value():
+    assert is_valid_telemetry_value("SQL") is False
+
 
 
