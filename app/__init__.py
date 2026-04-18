b7e50be4b6180f0f7b334811b60fb51cb2de17e6
Leheza Daniil <lehesadanylo27@gmail.com>
2026-04-18 17:48:02 +0300

    chore(architecture): init empty layered structure

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from app.db import db, jwt
-from app.routes.auth import auth
-from app.routes.locations import locations
-from app.routes.sensors import sensors
-from app.routes.telemetry import telemetry
+from app.presentation.routes.auth import auth
+from app.presentation.routes.locations import locations
+from app.presentation.routes.sensors import sensors
+from app.presentation.routes.telemetry import telemetry
 
 def create_app():
     app = Flask(__name__)
