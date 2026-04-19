937d11ad3dd3de8e405b7db01073513d77499d43
Leheza Daniil <lehesadanylo27@gmail.com>
2026-04-19 17:41:03 +0300

    chore(infrastructure): rename ORM models to more convenient names, e.g User -> UserModel

--- app/infrastructure/orm_models.py
+++ app/infrastructure/orm_models.py
@@ -1,21 +1,21 @@
 from app import db
 
-class User(db.Model):
+class UserModel(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(196), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
 
-class Location(db.Model):
+class LocationModel(db.Model):
     location_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
 
-class Sensor(db.Model):
+class SensorModel(db.Model):
     sensor_id = db.Column(db.Integer, primary_key=True)
     mac_address = db.Column(db.String(17), unique=True, nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey('location.location_id'), nullable=False)
 
-class Telemetry(db.Model):
+class TelemetryModel(db.Model):
     telemetry_id = db.Column(db.Integer, primary_key=True)
     sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.sensor_id'), nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
