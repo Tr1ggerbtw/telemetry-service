f82c13d94b8e9a55faeec2735b490a0336fec6bb
Leheza Daniil <lehesadanylo27@gmail.com>
2026-04-23 18:03:09 +0300

    fix(infrastructure): add __tablename__ to ORM models

--- app/infrastructure/orm_models.py
+++ app/infrastructure/orm_models.py
@@ -1,21 +1,25 @@
 from app import db
 
 class UserModel(db.Model):
+    __tablename__ = 'user'
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(196), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
 
 class LocationModel(db.Model):
+    __tablename__ = 'location'
     location_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
 
 class SensorModel(db.Model):
+    __tablename__ = 'sensor'
     sensor_id = db.Column(db.Integer, primary_key=True)
     mac_address = db.Column(db.String(17), unique=True, nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey('location.location_id'), nullable=False)
 
 class TelemetryModel(db.Model):
+    __tablename__ = 'telemetry'
     telemetry_id = db.Column(db.Integer, primary_key=True)
     sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.sensor_id'), nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
